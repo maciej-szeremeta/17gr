44ec36fac7f0d127a6d6993c80132bde75fb9705
Maciej-Szeremeta <maciej.szeremeta@o2.pl>
2022-07-26 18:54:48 +0200

    Create user model

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule, TypeOrmModuleOptions, } from '@nestjs/typeorm';
 import { config, } from 'ormconfig.autosync';
 import { AppController, } from './app.controller';
 import { AppService, } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ TypeOrmModule.forRoot(config as TypeOrmModuleOptions), ],
+  imports: [ TypeOrmModule.forRoot(config as TypeOrmModuleOptions), UserModule, ],
   controllers: [ AppController, ],
   providers: [ AppService, ],
 })
