137c90adcf30b428ee3daac05765844921148807
Maciej-Szeremeta <maciej.szeremeta@o2.pl>
2022-07-27 22:45:30 +0200

    Create all routes user_role

--- src/user_role/user_role.controller.ts
+++ src/user_role/user_role.controller.ts
@@ -1,7 +1,26 @@
-import { Controller, } from '@nestjs/common';
+import { Controller, Get, Param, Post, } from '@nestjs/common';
 
 @Controller('/user-role')
 export class UserRoleController {
 
-  // * POST
+  // * POST - Create a New user_role
+  // @ Admin
+  @Post('/')
+  createUserRole():string {
+    return 'create user role';
+  }
+
+  // * GET One User Role
+  // @ Admin
+  @Get('/:id')
+  getOneUserRole(@Param('id')id: string): string { 
+    return `get one user role id ${id}`;
+  }
+
+  // * GET All User Role
+  // @ Admin
+  @Get('/')
+  getListUserRole(): string { 
+    return 'get all user role';
+  }
 }
